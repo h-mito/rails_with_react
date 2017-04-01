b7c635940e627520b18062da416186fe372b7d95
mito <hiroshi.mito@gmail.com>
2017-04-02 01:20:01 +0900

    React で HelloWorld

---
+++ app/assets/javascripts/components/first_test.js.jsx
@@ -0,0 +1,12 @@
+
+class FirstTest extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <h2>Hello World!</h2>
+    );
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'first/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
