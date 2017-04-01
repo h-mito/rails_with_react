113e74df63edd7e9c723b84ab2e721adeb54ca80
mito <hiroshi.mito@gmail.com>
2017-04-02 03:38:46 +0900

    model作成、seedデータ(csv)作成、seed投入スクリプト作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('db/csv/boards.csv') do |row|
+  Board.create!(id: row[0], title: row[1], description: row[2])
+end
+
+CSV.foreach('db/csv/users.csv') do |row|
+  User.create!(id: row[0], name: row[1], sex: row[2], age: row[3])
+end
